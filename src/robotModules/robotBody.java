c4a210b438a9563b6588eb9085fa51fb97b8c030
cVASILOV <corneliuvasilov@gmail.com>
2018-09-27 16:43:22 +0200

    -changed the structure a bit

--- src/robotModules/robotBody.java
+++ src/robotModules/robotBody.java
@@ -4,26 +4,23 @@ import java.util.ArrayList;
 
 public class robotBody {
 
-	private ArrayList<robotModule> modules;
-
-	public ArrayList<robotModule> getModules() {
-		return modules;
-	}
-
-	public void createRobot(int nr_of_joints, int length_of_module, int with_of_module) {
-
-		for(int i = 0; i<nr_of_joints; i++){
-			robotModule temp = new robotModule();
-			modules.add(temp);
-		}
+	public static void main(String args[]) {
+		robotModule_top top_1 = new robotModule_top(250,300,100,100,50);
+		robotJoint joint_1 = new robotJoint(225,267);
+		robotModule_bottom bottom_1 = new robotModule_bottom(250,233,100,100,50);
+		top_1.setBottom(joint_1);
+		top_1.setTop(null);
+		joint_1.setTopModule(top_1);
+		joint_1.setBottomModule(bottom_1);
+		bottom_1.setTop(joint_1);
+		bottom_1.setBottom(null);
+		System.out.println(top_1.print());
+		System.out.println(joint_1.print());
+		System.out.println(bottom_1.print());
 		
-		for(robotModule robot : modules) {
-			robotJoint upperJoint = new robotJoint();
-			robotJoint lowerJoint = new robotJoint();
-			robot.setUpperJoint(upperJoint);
-			robot.setLowerJoint(lowerJoint);
-		}
 		
 	}
+
+
 	
 }
